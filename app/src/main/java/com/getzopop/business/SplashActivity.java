7be3bba5064c0d5858303907457a08c40002e8be
Vipin Kumar Thakur <vipin12thakur@gmail.com>
2020-04-19 13:17:08 +0530

    Attached views to class

--- app/src/main/java/com/getzopop/business/SplashActivity.java
+++ app/src/main/java/com/getzopop/business/SplashActivity.java
@@ -1,13 +1,19 @@
 package com.getzopop.business;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
 
 import com.getzopop.business.auth.MobileActivity;
 import com.getzopop.business.util.AppConstant;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SplashActivity extends AppCompatActivity {
@@ -26,14 +32,23 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                if (firebaseAuth.getCurrentUser() != null){
-                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                    finish();
-                } else {
-                    startActivity(new Intent(SplashActivity.this, MobileActivity.class));
-                    finish();
-                }
-
+                firebaseAuth.signInAnonymously()
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(SplashActivity.this, ""+firebaseAuth.getCurrentUser().getUid(), Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(SplashActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         }, AppConstant.SPLASH_DELAY);
     }
